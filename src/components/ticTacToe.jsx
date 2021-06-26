300a74a47402ee4112b85417b872bd1589c2e975
Aditya Mahale <aditya.mahale@enterprisedb.com>
2021-06-26 13:11:52 +0530

    Simplify content object

--- src/components/ticTacToe.jsx
+++ src/components/ticTacToe.jsx
@@ -5,9 +5,9 @@ import Unit from './unit';
 class TicTacToe extends Component {
     state = {
         content: [
-          [{id: 1, content: ""}, {id: 2, content: ""}, {id: 3, content: ""}],
-          [{id: 4, content: ""}, {id: 5, content: ""}, {id: 6, content: ""}],
-          [{id: 7, content: ""}, {id: 8, content: ""}, {id: 9, content: ""}],
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
         ],
         player: 'X',
         winner: ''
@@ -15,15 +15,12 @@ class TicTacToe extends Component {
     
     handleClick = (row, col) => {
         const { content: allContent, player, winner } = this.state;
-        if (col.content) return;
+        if (allContent[row][col]) return;
         if (winner) return;
         
         const content = [...allContent];
-        const rowIndex = content.indexOf(row);
-        content[rowIndex] = [...content[rowIndex]];
-        const colIndex = content[rowIndex].indexOf(col);
-        content[rowIndex][colIndex] = {...content[rowIndex][colIndex]};
-        content[rowIndex][colIndex].content = player;
+        content[row] = [...content[row]];
+        content[row][col] = player;
         
         this.setState({content, player: player === 'X' ? 'O' : 'X'});
         this.findWinner();
@@ -31,23 +28,19 @@ class TicTacToe extends Component {
 
     handleReset = () => {
         const content = this.state.content.map(
-            row => row.map(
-                col => { 
-                    return { ...col, content: "" }
-                }
-            )
+            row => row.map(() => "")
         );
-        this.setState({ content, winner: '' });
+        this.setState({ content, winner: '', player: 'X' });
     };
 
     findWinner = () => {
         const { content } = this.state;
         const getColumn = (i) => content.map(row => row[i]);
         for (let i = 0; i < 3; i++) {
-            if (content[i][0].content && content[i].every(e => content[i][0].content === e.content)) 
-                return this.setState({ winner:  content[i][0].content });
-            if (content[0][i].content && getColumn(i).every(e => content[0][i].content === e.content)) 
-                return this.setState({ winner:  content[0][i].content });
+            if (content[i][0] && content[i].every(e => content[i][0] === e)) 
+                return this.setState({ winner:  content[i][0] });
+            if (content[0][i] && getColumn(i).every(e => content[0][i] === e)) 
+                return this.setState({ winner:  content[0][i] });
         }
     }
 
@@ -65,8 +58,8 @@ class TicTacToe extends Component {
                                         (col, j) => 
                                             <div key={j} className="ml-2">
                                                 <Unit 
-                                                    content={col.content} 
-                                                    onClick={() => this.handleClick(row, col)} 
+                                                    content={col} 
+                                                    onClick={() => this.handleClick(i, j)} 
                                                 />
                                             </div>)
                                     }
